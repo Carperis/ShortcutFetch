73166207b017c9eabf96741a8ab27b4e4b910b87
Carperis <samqiupeng@hotmail.com>
2023-04-01 14:11:03 -0400

    new start

---
+++ test.py
@@ -0,0 +1,35 @@
+# https://flask-json.readthedocs.io/en/latest/
+
+from datetime import datetime
+from flask import Flask, request
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+
+app = Flask(__name__)
+FlaskJSON(app)
+
+
+@app.route('/get_time')
+def get_time():
+    now = datetime.utcnow()
+    return json_response(time=now)
+
+
+@app.route('/increment_value', methods=['POST'])
+def increment_value():
+    # We use 'force' to skip mimetype checking to have shorter curl command.
+    data = request.get_json(force=True)
+    try:
+        value = int(data['value'])
+    except (KeyError, TypeError, ValueError):
+        raise JsonError(description='Invalid value.')
+    return json_response(value=value + 1)
+
+
+@app.route('/get_value')
+@as_json
+def get_value():
+    return dict(value=12)
+
+
+if __name__ == '__main__':
+    app.run()
